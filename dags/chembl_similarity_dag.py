9e20c42a1340afbd24515e3834e02b659adb56b9
Nano_Karbaia <nanokarbaia@gmail.com>
2026-07-20 20:52:07 +0400

    Add project skeleton and local infrastructure

---
+++ dags/chembl_similarity_dag.py
@@ -0,0 +1,87 @@
+"""Airflow DAG for the ChEMBL molecule similarity pipeline."""
+
+from __future__ import annotations
+
+from datetime import timedelta
+
+import pendulum
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import DAG, Param
+from airflow.utils.trigger_rule import TriggerRule
+
+from lib.chembl.constants import (
+    DEFAULT_CHEMBL_PAGE_LIMIT,
+    DEFAULT_SOURCE_MOLECULE_LIMIT,
+    DEFAULT_TOP_N,
+)
+from lib.utils.teams import send_teams_alert
+
+
+with DAG(
+    dag_id='chembl_similarity_dag',
+    description='Ingest ChEMBL data, compute molecule similarities, and build data mart views.',
+    schedule=None,
+    start_date=pendulum.datetime(2026, 1, 1, tz='UTC'),
+    catchup=False,
+    tags=['chembl', 'similarity', 'rdkit', 'de_school'],
+    params={
+        'chembl_page_limit': Param(
+            default=DEFAULT_CHEMBL_PAGE_LIMIT,
+            type='integer',
+            minimum=1,
+            description='Number of records to request per ChEMBL API page.',
+        ),
+        'ingest_record_limit': Param(
+            default=None,
+            type=['null', 'integer'],
+            description='Optional development limit for ingested molecule records. Use null for full ingestion.',
+        ),
+        'source_molecule_limit': Param(
+            default=DEFAULT_SOURCE_MOLECULE_LIMIT,
+            type='integer',
+            minimum=1,
+            description='Number of source molecules for top-N similarity search.',
+        ),
+        'top_n': Param(
+            default=DEFAULT_TOP_N,
+            type='integer',
+            minimum=1,
+            description='Number of most similar molecules to keep per source molecule.',
+        ),
+    },
+    dagrun_timeout=timedelta(hours=6),
+    default_args={
+        'owner': 'data-platform',
+        'retries': 1,
+        'retry_delay': timedelta(minutes=2),
+        'retry_exponential_backoff': True,
+        'max_retry_delay': timedelta(minutes=30),
+        'on_failure_callback': send_teams_alert,
+    },
+) as dag:
+    start_op = EmptyOperator(task_id='start')
+
+    ingest_chembl_data_op = EmptyOperator(task_id='ingest_chembl_data')
+    prepare_silver_layer_op = EmptyOperator(task_id='prepare_silver_layer')
+    compute_fingerprints_op = EmptyOperator(task_id='compute_fingerprints')
+    compute_similarity_scores_op = EmptyOperator(task_id='compute_similarity_scores')
+    extract_top10_similarities_op = EmptyOperator(task_id='extract_top10_similarities')
+    build_data_mart_op = EmptyOperator(task_id='build_data_mart')
+    create_views_op = EmptyOperator(task_id='create_views')
+
+    finish_op = EmptyOperator(
+        task_id='finish',
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
+    )
+
+    (
+        start_op
+        >> ingest_chembl_data_op
+        >> prepare_silver_layer_op
+        >> compute_fingerprints_op
+        >> compute_similarity_scores_op
+        >> extract_top10_similarities_op
+        >> build_data_mart_op
+        >> create_views_op
+        >> finish_op
+    )
---
+++ dags/lib/chembl/constants.py
@@ -0,0 +1,25 @@
+"""Constants for the ChEMBL similarity pipeline."""
+
+from __future__ import annotations
+
+import os
+
+DWH_CONN_ID = 'dwh_postgres'
+AWS_CONN_ID = 'aws_s3'
+MSTEAMS_CONN_ID = 'msteams_webhook'
+
+S3_BUCKET = os.getenv('S3_BUCKET', 'de-school-educational-data')
+S3_BASE_PREFIX = os.getenv('S3_BASE_PREFIX', 'final_task/karbaia_nano').strip('/')
+
+BRONZE_SCHEMA = 'bronze'
+SILVER_SCHEMA = 'silver'
+GOLD_SCHEMA = 'gold'
+
+CHEMBL_API_BASE_URL = 'https://www.ebi.ac.uk/chembl/api/data'
+
+FINGERPRINT_RADIUS = 2
+FINGERPRINT_N_BITS = 2048
+
+DEFAULT_CHEMBL_PAGE_LIMIT = 1000
+DEFAULT_SOURCE_MOLECULE_LIMIT = 100
+DEFAULT_TOP_N = 10
